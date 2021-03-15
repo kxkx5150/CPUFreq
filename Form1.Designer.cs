b9ff7492d36367613cc04609de09d40592e34f9f
Kunihiro Ando <kxkx5150@users.noreply.github.com>
2021-03-15 16:44:26 +0900

    commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -25,7 +25,27 @@ namespace CPUFreq {
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
@@ -34,26 +54,191 @@ namespace CPUFreq {
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            resources.ApplyResources(this.numericUpDown1, "numericUpDown1");
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // numericUpDown2
+            // 
+            resources.ApplyResources(this.numericUpDown2, "numericUpDown2");
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Lime;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Orange;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(256, 140);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.Text = "CPUFreq";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,9 +4,11 @@ using System.Management;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Microsoft.Win32;
 
 namespace CPUFreq {
     public partial class Form1 : Form {
+        public bool cpuUsage = false;
         public int interval = 1000;
         public bool showMemory = true;
         public int memCount = 5;
@@ -16,18 +18,65 @@ namespace CPUFreq {
         private int maxFreq;
         private int count = -1;
         private NotifyIcon notifyIcon1 = new NotifyIcon();
+        private PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto)]
+        extern static bool DestroyIcon(IntPtr handle);
 
-        private void getOptions() {
-            interval = 1000;
-            showMemory = true;
-            memCount = 5;
-            cpuColor = Color.Lime;
-            memColor = Color.DeepPink;
-
-
-
-
+        private void timer1_Tick(object sender, EventArgs e) {
+            getInfo();
         }
+        public void CreateTextIcon(string str, Color color) {
+            SolidBrush brush = new SolidBrush(color);
+            Font fontToUse = new Font("MS Gothic", 14, FontStyle.Bold, GraphicsUnit.Pixel);
+            Bitmap bitmapText = new Bitmap(16, 16);
+
+            Graphics g = Graphics.FromImage(bitmapText);
+            g.FillRectangle(Brushes.Black, g.VisibleClipBounds);
+            g.DrawString(str, fontToUse, brush, -1, 1);
+            IntPtr Hicon = bitmapText.GetHicon();
+            Icon newIcon = Icon.FromHandle(Hicon);
+            notifyIcon1.Icon = newIcon;
+
+            brush.Dispose();
+            fontToUse.Dispose();
+            bitmapText.Dispose();
+            g.Dispose();
+            newIcon.Dispose();
+            DestroyIcon(newIcon.Handle);
+        }
+        private void getOptions() {
+            cpuUsage = Properties.Settings.Default._cpuUsage;
+            interval = Properties.Settings.Default._interval;
+            showMemory = Properties.Settings.Default._showMemory;
+            memCount = Properties.Settings.Default._memCount;
+            cpuColor = Properties.Settings.Default._cpuColor;
+            memColor = Properties.Settings.Default._memColor;
+        }
+        private void setOptions() {
+            Properties.Settings.Default._cpuUsage = cpuUsage;
+            Properties.Settings.Default._interval = interval;
+            Properties.Settings.Default._showMemory = showMemory;
+            Properties.Settings.Default._memCount = memCount;
+            Properties.Settings.Default._cpuColor = cpuColor;
+            Properties.Settings.Default._memColor = memColor;
+            Properties.Settings.Default.Save();
+        }
+        private void setFormOptions() {
+            if (cpuUsage) {
+                comboBox1.SelectedIndex = 1;
+            } else {
+                comboBox1.SelectedIndex = 0;
+            }
+            numericUpDown1.Value = interval;
+            checkBox1.Checked = showMemory;
+            numericUpDown2.Value = memCount;
+            label5.BackColor = cpuColor;
+            label6.BackColor = memColor;
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+            setFormOptions();
+        }
+
         public Form1() {
             this.ShowInTaskbar = false;
             getOptions();
@@ -54,39 +103,27 @@ namespace CPUFreq {
             notifyIcon1.ContextMenuStrip = menu;
             notifyIcon1.DoubleClick += new EventHandler(NotifyIcon1_DoubleClick);
         }
-
+        private void changeInteval() {
+            timer1.Stop();
+            timer1.Interval = interval;
+            timer1.Start();
+        }
         private void getInfo() {
             count++;
+
             if (count == 0) {
-                timer1.Stop();
-                timer1.Interval = interval;
-                timer1.Start();
+                changeInteval();
             } else if (showMemory && count % memCount == 0) {
                 count = 0;
                 int mem = getMemoryInfo();
                 CreateTextIcon(mem.ToString("D2"), memColor);
+            }else if (cpuUsage) {
+                int usage = getCPUUsage();
+                CreateTextIcon(usage.ToString("D2"), cpuColor);
             } else {
-                int tval = timer1.Interval;
                 int frq = getCPUFrequency();
                 CreateTextIcon(frq.ToString("D2"), cpuColor);
             }
-        }
-        public void CreateTextIcon(string str, Color color) {
-            Font fontToUse = new Font("MS Gothic", 13, FontStyle.Bold, GraphicsUnit.Pixel);
-            Brush brushToUse = new SolidBrush(color);
-            Bitmap bitmapText = new Bitmap(16, 16);
-            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
-
-            g.Clear(Color.Black);
-            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
-            g.DrawString(str, fontToUse, brushToUse, -1, 2);
-            IntPtr hIcon = (bitmapText.GetHicon());
-            notifyIcon1.Icon = System.Drawing.Icon.FromHandle(hIcon);
-
-            g.Dispose();
-            bitmapText.Dispose();
-            brushToUse.Dispose();
-            fontToUse.Dispose();
         }
 
         private int getMemoryInfo() {
@@ -95,6 +132,9 @@ namespace CPUFreq {
             decimal percentFree = ((decimal)phav / (decimal)tot) * 100;
             decimal percentOccupied = 100 - percentFree;
             return Convert.ToInt32(percentOccupied);
+        }
+        private int getCPUUsage() {
+            return Convert.ToInt32(cpuCounter.NextValue());
         }
 
         private int getCPUFrequency() {
@@ -113,9 +153,6 @@ namespace CPUFreq {
             cpuEnum.MoveNext();
             return System.Int32.Parse(cpuEnum.Current["MaxClockSpeed"].ToString());
         }
-        private void timer1_Tick(object sender, EventArgs e) {
-            getInfo();
-        }
         private void Close_Click(object sender, EventArgs e) {
             notifyIcon1.Visible = false;
             Application.Exit();
@@ -131,6 +168,66 @@ namespace CPUFreq {
                 e.Cancel = true;
                 Hide();
             }
+        }
+
+        private void label5_Click(object sender, EventArgs e) {
+            DialogResult dr = colorDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK) {
+                cpuColor = colorDialog1.Color;
+                label5.BackColor = colorDialog1.Color;
+                setOptions();
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e) {
+            DialogResult dr = colorDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK) {
+                memColor = colorDialog1.Color;
+                label6.BackColor = colorDialog1.Color;
+                setOptions();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            if (checkBox1.Checked) {
+                groupBox1.Show();
+                showMemory = true;
+            } else {
+                groupBox1.Hide();
+                showMemory = false;
+            }
+            setOptions();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) {
+            interval = Decimal.ToInt32(numericUpDown1.Value);
+            setOptions();
+            changeInteval();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e) {
+            memCount = Decimal.ToInt32(numericUpDown2.Value);
+            count = 0;
+            setOptions();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            rkApp.SetValue("CPUFreq_ando", Application.ExecutablePath);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            rkApp.DeleteValue("CPUFreq_ando", false);
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e) {
+            if(comboBox1.SelectedIndex == 0) {
+                cpuUsage = false;
+            } else {
+                cpuUsage = true;
+            }
+            setOptions();
         }
     }
     public static class PerformanceInfo {
